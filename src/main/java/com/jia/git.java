c9281a3773934925dcecfcc9eca4b23579831225
Kramir <3221559584@qq.com>
2023-05-25 14:11:53 +0800

    second commit

--- src/main/java/com/jia/git.java
+++ src/main/java/com/jia/git.java
@@ -3,5 +3,7 @@ package com.jia;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello.git!");
+        System.out.println("hello.git2!");
+        System.out.println("hello.git3!");
     }
 }
